e6fbb44a96ae6c4823b52dc553ec3caaccfd2d1c
TedoHarisC <tedoharischandra29@gmail.com>
2022-07-07 15:04:11 +0700

    Menambahkan custom bottom nav

---
+++ lib/pages/home/main_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget cartButton() {
+      return FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: secondaryColor,
+        child: Image.asset(
+          'assets/icon_cart.png',
+          width: 20,
+        ),
+      );
+    }
+
+    Widget customButtonNav() {
+      return ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          color: backgroundColor4,
+          notchMargin: 6,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            backgroundColor: backgroundColor4,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_home.png',
+                  width: 18,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_chat.png',
+                  width: 18,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_wishlist.png',
+                  width: 18,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_profile.png',
+                  width: 18,
+                ),
+                label: '',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      floatingActionButton: cartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customButtonNav(),
+      body: const Center(
+        child: Text('Main Page'),
+      ),
+    );
+  }
+}
