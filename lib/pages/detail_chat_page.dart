6b11af08344295f607d10f16d786c82d95180797
TedoHarisC <tedoharischandra29@gmail.com>
2022-07-11 13:01:22 +0700

    Slicing detail button

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -45,9 +45,45 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget chatInput() {
+      return Container(
+        margin: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 45,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: backgroundColor4,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: TextFormField(
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Type Message...',
+                      hintStyle: subtitleTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 12),
+            Image.asset(
+              'assets/btn_send.png',
+              width: 45,
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
+      bottomNavigationBar: chatInput(),
     );
   }
 }
